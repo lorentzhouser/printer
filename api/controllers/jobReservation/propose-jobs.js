8327be1257677396b38b9e5eec38239f1d980ac7
Lorentz <lorentzhouser@me.com>
2020-05-26 01:25:05 +0200

    successful basic next day recommendation

--- api/controllers/jobReservation/propose-jobs.js
+++ api/controllers/jobReservation/propose-jobs.js
@@ -78,11 +78,57 @@ module.exports = {
         }
         
         //add end of last job possibility 
-        allPrintSlots.push([-1, lastJobInOrder.date + lastJobInOrder.duration + margin]);
+        allPrintSlots.push([-1, lastJobInOrder.date + lastJobInOrder.duration]); //+margin
 
         //reduce slots to usable slots
         const printSlots = allPrintSlots.filter(slot => {
           return (slot[0] >= reservationDuration || slot[0] == -1);
+        });
+
+        console.log('slots available: ' + printSlots.length);
+
+        //recommendedSlots sorted by date
+        var recommendedSlots = [];
+        printSlots.forEach((slot,index) => {
+          const slotDate = new Date(0);
+          slotDate.setUTCSeconds(slot[1]);
+          const slotHrs = slotDate.getHours();
+          if (slotHrs >= startingWorkHour && slotHrs <= finishingWorkHour) {
+            recommendedSlots.push(slot);
+          }
+          else if (index < printSlots.length-1) {
+            const nextSlotDate = new Date(1970, 0, 1); // Epoch
+            nextSlotDate.setSeconds(printSlots[index+1][1]);
+            const nextSlotHrs = slotDate.getHours();
+            if (nextSlotHrs > startingWorkHour) {
+              //get end-of-print hour
+              const endSlotDate = new Date(1970, 0, 1); // Epoch
+              endSlotDate.setSeconds(Number(slot[1]) + Number(slot[0]));
+              const slotEndHrs = endSlotDate.getHours();
+
+              const startWorkDate = nextSlotDate;
+              startWorkDate.setSeconds(0);
+              startWorkDate.setMinutes(0);
+              startWorkDate.setHours(startingWorkHour);
+              
+              if (slotEndHrs > startingWorkHour) {
+                //check time between this print and next
+                const delta_time = nextSlotDate.getUTCSeconds() - endSlotDate.getUTCSeconds();
+                if (delta_time >= reservationDuration) {
+                  console.log('cons 2');
+                  recommendedSlots.push([delta_time, endSlotDate.getUTCSeconds()]);
+                }
+              }
+              else {
+                //check time between startingWorkHour and nextTime
+                const delta_time = nextSlotDate.getUTCSeconds() - startWorkDate.getUTCSeconds();
+                if (delta_time >= reservationDuration) {
+                  console.log('cons 3');
+                  recommendedSlots.push([delta_time, startingWorkDate.getUTCSeconds()]);
+                }
+              }
+            }
+          }
         });
 
         //urgent means get first usable slot
@@ -103,8 +149,33 @@ module.exports = {
         -? should the array be [shortest, greatest, shortest+1, greatest-1, ...]
           -? need to find more standard data for average print time etc..
         */
-        const printSlotsByDuration = printSlots.sort((first, second) => { return (first[0] - second[0]); });
-        recommendJobStart = printSlotsByDuration[0][1];
+
+        if (recommendedSlots.length == 0) {
+          console.log('no recommendations yet!');
+          
+          // getLastSlotTime
+          const lastSlot = printSlots.filter(slot => { return (slot[0]==-1); });
+          if (lastSlot.length>1) { console.log ('should only have one slot with duration -1'); }
+          const endLastSlotDate = new Date(1970, 0, 1); // Epoch
+          endLastSlotDate.setSeconds(Number(lastSlot[0][0]) + Number(lastSlot[0][1]));
+          const lastSlotEndHours = endLastSlotDate.getHours();
+
+          const lastResortDate = new Date(endLastSlotDate.getFullYear(), endLastSlotDate.getMonth(), endLastSlotDate.getDate()); // Epoch
+          lastResortDate.setHours(7);
+          if (endLastSlotDate < 0 || lastSlotEndHours > startingWorkHour) {
+            console.log('next day');
+            lastResortDate.setDate(endLastSlotDate.getDate() + 1);
+          }
+          recommendJobStart = lastResortDate.getTime()/1000; //convert to seconds from millis -> divide by 1000 
+        }
+        else {
+          const recommendedSlotsByDuration = recommendedSlots.sort((first, second) => { return (first[0] - second[0]); });
+          recommendedSlotsByDuration.forEach(slot => {
+            console.log(slot[0] + ' ' + slot[1]);
+          });
+
+          recommendJobStart = recommendedSlotsByDuration[0][1];
+        }
       }
 
       //create object time slot {printer: 1, time: recommendedJobStart};
