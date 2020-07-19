2aa073809d568b193378509c276f04f642259082
Lorentz <lorentzhouser@me.com>
2020-07-19 02:59:06 +0200

    Simplified & vueified gcode processing code.

---
+++ frontend/src/components/ReservationModule/Confirmation.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="Confirmation">
+        <div class="receipt">
+            <div id="confirm-schedule" class="subtitle">
+                {{receipt.startTime}}
+            </div>
+            <div id="confirm-printer" class="caption">
+                printer {{receipt.printer}}
+            </div>
+            <div id="confirm-filament" class="caption">
+                {{receipt.filament}} mm
+            </div>
+        </div>
+        <div @click="confirmReservation" id="reserve-button" class="confirm Button">RESERVE</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Confirmation',
+    //probably passed as props
+    props: ['receipt'],
+    methods: {
+        confirmReservation: function() {
+            this.$router.push('/job-reservations');
+            console.log('redirect after server post request');
+
+            // const postURL = '/api/v1/reserve-job';
+
+            // if ((this.getDuration() == -1) || (this.getSelectedReservationValue() == -1)) {
+            //     console.log("one date variable has not been initialized");
+            //     return;
+            // }
+
+            // var formData = new FormData()
+            // formData.set('duration', this.getDuration());
+            // formData.set('date', this.getSelectedReservationValue().startTime); 
+            // formData.set('description', this.getFilename());
+            // formData.set('class', this.getClassRelatedStatus());
+            // formData.set('device', this.getSelectedReservationValue().device);
+
+            // //must address cross-site post issue (for refresh of site)?
+            // let request = new XMLHttpRequest();
+            // request.open("POST", postURL);
+            // request.send(formData);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "@/assets/css/components/_reservation-module.scss";
+</style>
---
+++ frontend/src/components/ReservationModule/Options.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="PrintOptions" v-if="Object.keys(this.proposal).length !== 0">
+        <div class="MenuOption">
+        <div class="InfoTextOption" @click="selectOption('recommended')">
+            <div class="subtitle2">Recommended</div>
+            <div class="caption">{{proposal.recommended.startTime}}</div>
+        </div>
+        </div>
+        <div class="MenuOption" @click="selectOption('urgent')">
+        <div class="InfoTextOption">
+            <div class="subtitle2">Urgent</div>
+            <div class="caption">{{proposal.urgent.startTime}}</div>
+        </div>
+        </div>
+        <div class="MenuOption" @click="other">
+        <div class="InfoTextOption">
+            <div class="subtitle2">Other</div>
+            <div class="caption">static what?</div>
+        </div>
+        </div>
+        
+        <div class="PrivateOption"><input v-model="checked" type="checkbox">  Course Related</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Options',
+    props: ['proposal'],
+    data: function() {
+        return {
+            checked: false
+        }
+    },
+    methods: {
+        selectOption: function(option) {
+            this.$emit('selectOption', option)
+        },
+        other: function() {
+            //push with file data
+            //drag
+            this.$router.push('/job-reservations');
+            this.$emit('hide');
+        }
+    },
+    watch: { 
+        checked: function() {
+            this.$emit('makeCourseRelated', this.checked);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "@/assets/css/components/_reservation-module.scss";
+</style>
