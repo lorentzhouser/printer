e2b1f527e10081bec5c53b8500d61f6f82845b5e
Lorentz <lorentzhouser@me.com>
2021-03-08 17:02:38 +0100

    needs clean up and clarity

--- frontend/src/views/JobReservations.vue
+++ frontend/src/views/JobReservations.vue
@@ -3,10 +3,10 @@
         <div class="TempNav">
             <div class="PageTitle h1">Printer Reservation</div>
             <div class="UserInfo">
-                <button v-if="reservationInProcess==false" class="button small NavButton"><span>Printer Error</span></button>
-                <button v-if="reservationInProcess==false" class="button small NavButton"><span>Contact Author</span></button>
-                <button v-if="reservationInProcess==false" class="button small NavButton"><span>Request Slot</span></button>
-                <button v-if="reservationInProcess==false" class="button small NavButton"><span>Cancel Reservation</span></button>
+                <button v-if="selectedJob!=-1 && reservationInProcess==false" class="button small NavButton"><span>Printer Error</span></button>
+                <button v-if="selectedJob!=-1 && selectedJob.user != this.userId" class="button small NavButton"><span>Contact Author</span></button>
+                <button v-if="selectedJob!=-1 && selectedJob.user != this.userId" class="button small NavButton"><span>Request Slot</span></button>
+                <button v-if="cancelable" class="button small NavButton"><span>Cancel Reservation</span></button>
 
                 <button v-if="reservationInProcess==false" class="button small NavButton" @click="openReservationModal">
                     <span>+</span>
@@ -32,7 +32,7 @@
 
             <div class="Queue" v-bind:key="printerQueue.device" v-for="printerQueue in computedPrinterQueues">
                 <div class="LeftColumnTitle">{{printerQueue.device}}</div>
-                <div v-dragged="onDragged" class="Job" v-bind:key="job.id" v-for="job in printerQueue.jobs" v-bind:class="job.priority" v-bind:style="{ width: job.widthPercentage, left: job.left}">
+                <div v-dragged="onDragged" @click="selectJob(job)"  class="Job" v-bind:key="job.id" v-for="job in printerQueue.jobs" v-bind:class="job.priority" v-bind:style="{ width: job.widthPercentage, left: job.left}">
                     <div class="StartTime" v-if="job.priority == 'New'">
                         <span>{{startTimeHumanReadable}}</span>
                     </div>
@@ -60,162 +60,175 @@ export default {
       return {
         startTop: -1,
         startTimeHumanReadable: "",
+        selectedJob: -1,
       }
     },
     methods: {
-    convertPositionToDate: function(left) {
-        const hours = left/100;
-        const secondsFromLeft = hours*3600;
+        selectJob: function(job) {
+            // if (job.user == this.userId) {
+                this.selectedJob = job;
+            // }
+        },
+        convertPositionToDate: function(left) {
+            const hours = left/100;
+            const secondsFromLeft = hours*3600;
 
-        const startDate = new Date(0);
-        startDate.setUTCSeconds(Date.now()/1000);
-        startDate.setHours(startDate.getHours());
-        startDate.setMinutes(0);
-        startDate.setSeconds(0);
-        
-        return ((startDate.getTime()/1000) + secondsFromLeft);    
-    },
-    humanReadableTime: function(left) {
-        const absoluteDateSeconds = this.convertPositionToDate(left);
-        // console.log("absolute seconds: " + absoluteDateSeconds);
-        const date = new Date(0);
-        date.setUTCSeconds(absoluteDateSeconds);
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        if (hours < 10) {
-            hours = '0' + hours;
-        }
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-        this.startTimeHumanReadable = hours+":"+minutes;
-    },
-    openReservationModal: function() {
-        this.$store.dispatch("showModal");
-    },
-    confirmReservation: function() {
-        console.log("confirm reservation  new date: " + this.newJob.date);
-        this.$store.dispatch("reserveJob", this.newJob);
-        console.log("confirm");
-    },
-    cancelReservation: function() {
-        this.$store.dispatch("queryJobs");
-    },
-    onDragged({ el, deltaX, deltaY, _clientX, _clientY, _offsetX, _offsetY, first, last }) {
-        // QUIT IF NOT A NEW JOB PROPOSAL
-        var shouldContinue = false;
-        el.classList.forEach((className) => {
-            if (className == "New") { shouldContinue = true; }
-        })
-        if (shouldContinue ==  false) { return; }
+            const startDate = new Date(0);
+            startDate.setUTCSeconds(Date.now()/1000);
+            startDate.setHours(startDate.getHours());
+            startDate.setMinutes(0);
+            startDate.setSeconds(0);
+            
+            return ((startDate.getTime()/1000) + secondsFromLeft);    
+        },
+        humanReadableTime: function(left) {
+            const absoluteDateSeconds = this.convertPositionToDate(left);
+            // console.log("absolute seconds: " + absoluteDateSeconds);
+            const date = new Date(0);
+            date.setUTCSeconds(absoluteDateSeconds);
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            this.startTimeHumanReadable = hours+":"+minutes;
+        },
+        openReservationModal: function() {
+            this.selectedJob = -1;
+            this.$store.dispatch("showModal");
+        },
+        confirmReservation: function() {
+            console.log("confirm reservation  new date: " + this.newJob.date);
+            this.$store.dispatch("reserveJob", this.newJob);
+            console.log("confirm");
+        },
+        cancelReservation: function() {
+            this.$store.dispatch("queryJobs");
+        },
+        onDragged({ el, deltaX, deltaY, _clientX, _clientY, _offsetX, _offsetY, first, last }) {
+            // QUIT IF NOT A NEW JOB PROPOSAL
+            var shouldContinue = false;
+            el.classList.forEach((className) => {
+                if (className == "New") { shouldContinue = true; }
+            })
+            if (shouldContinue ==  false) { return; }
 
-        // BULLSHIT
-        if ((_clientX == (1/_clientY)) && (_offsetX == _offsetY)) {console.log('surprise');}
-        // BULLSHIT
+            // BULLSHIT
+            if ((_clientX == (1/_clientY)) && (_offsetX == _offsetY)) {console.log('surprise');}
+            // BULLSHIT
 
-        var l = +window.getComputedStyle(el)['left'].slice(0, -2) || 0;
-        var t = +window.getComputedStyle(el)['top'].slice(0, -2) || 0;
+            var l = +window.getComputedStyle(el)['left'].slice(0, -2) || 0;
+            var t = +window.getComputedStyle(el)['top'].slice(0, -2) || 0;
 
-        if (first) {
-            this.startTop = el.style.top;
-            this.printerQueues.forEach(queue => {  
-                queue.jobs.forEach(job => {
-                    if (job.priority == "New") {
-                        this.job = job;
-                        // this.device = queue.device;
+            if (first) {
+                this.startTop = el.style.top;
+                this.printerQueues.forEach(queue => {  
+                    queue.jobs.forEach(job => {
+                        if (job.priority == "New") {
+                            this.job = job;
+                            // this.device = queue.device;
+                        }
+                    });
+                });
+                
+                el.style.zIndex = "500";
+                return
+            }
+            if (last) { //COMPLETE DRAG
+                
+                var deltaYvertical = parseFloat(window.getComputedStyle(el).top);
+                //THERE IS A difference between moving one space and moving two!
+
+                var distanceToOvercome = parseFloat(window.getComputedStyle(el.parentNode).height)/2+ parseFloat(window.getComputedStyle(el.parentNode).marginBottom)/2;
+                var rowChange = 0;
+                var sign = deltaYvertical < 0 ? -1 : 1;
+                deltaYvertical = Math.abs(deltaYvertical);
+                while (deltaYvertical > distanceToOvercome) {
+                    rowChange++;
+                    distanceToOvercome += parseFloat(window.getComputedStyle(el.parentNode).height)+ parseFloat(window.getComputedStyle(el.parentNode).marginBottom);
+                }
+                rowChange *= sign;
+                const lastDevice = this.newJob.device;
+                var newDevice = lastDevice + rowChange;
+                if (newDevice < 1) {
+                    newDevice = 1;
+                }
+                else  if (newDevice > this.printerQueues.length) {
+                    newDevice = this.printerQueues.length;
+                }
+
+                // console.log('deltaY ' + deltaYvertical);
+                // console.log('rowChange ' + rowChange);
+
+                // get job from priority, get device, get new device from transformation
+                
+                var updatedJob = this.newJob;
+                updatedJob.device = newDevice;
+                updatedJob.date = this.convertPositionToDate(parseFloat(el.style.left));
+                
+                if (lastDevice == newDevice) {
+                    console.log("no queue change");
+                    el.style.top = this.startTop;
+                }
+            // else {
+                var updatedQueues = this.printerQueues;
+                var remainingJobs;
+
+                //get jobs regular jobs
+                this.printerQueues.forEach((queue) => {
+                    if (queue.device == lastDevice) {
+                        remainingJobs = queue.jobs.filter((job) => {
+                            return (job.priority != "New");
+                        });
                     }
                 });
-            });
-            
-            el.style.zIndex = "500";
-            return
-        }
-        if (last) { //COMPLETE DRAG
-            
-            var deltaYvertical = parseFloat(window.getComputedStyle(el).top);
-            //THERE IS A difference between moving one space and moving two!
 
-            var distanceToOvercome = parseFloat(window.getComputedStyle(el.parentNode).height)/2+ parseFloat(window.getComputedStyle(el.parentNode).marginBottom)/2;
-            var rowChange = 0;
-            var sign = deltaYvertical < 0 ? -1 : 1;
-            deltaYvertical = Math.abs(deltaYvertical);
-            while (deltaYvertical > distanceToOvercome) {
-                rowChange++;
-                distanceToOvercome += parseFloat(window.getComputedStyle(el.parentNode).height)+ parseFloat(window.getComputedStyle(el.parentNode).marginBottom);
+                console.log("remaining job count: " + remainingJobs.length);
+
+                //update previous and new queue
+                updatedQueues.forEach((queue) => {
+                    if (queue.device == lastDevice) {
+                        queue.jobs = remainingJobs;
+                    }
+                    if (queue.device == newDevice) {
+                        queue.jobs.push(this.newJob);
+                    }
+                });
+                // this.printerQueues = updatedQueues;
+                this.$store.dispatch("updateQueues", updatedQueues);
+            // }
+
+
+                this.$store.dispatch("updateNewJob", updatedJob.date);
+
+                //RESET Z-Index after animation
+                const jobStyle = el.style;
+                jobStyle.zIndex = "0";
+                
+                this.startTimeHumanReadable = '';
+                
+                return;
             }
-            rowChange *= sign;
-            const lastDevice = this.newJob.device;
-            var newDevice = lastDevice + rowChange;
-            if (newDevice < 1) {
-                newDevice = 1;
-            }
-            else  if (newDevice > this.printerQueues.length) {
-                newDevice = this.printerQueues.length;
-            }
-
-            // console.log('deltaY ' + deltaYvertical);
-            // console.log('rowChange ' + rowChange);
-
-            // get job from priority, get device, get new device from transformation
+            // el.style.left = l + deltaX + 'px';
+            //Update date
             
-            var updatedJob = this.newJob;
-            updatedJob.device = newDevice;
-            updatedJob.date = this.convertPositionToDate(parseFloat(el.style.left));
-            
-            if (lastDevice == newDevice) {
-                console.log("no queue change");
-                el.style.top = this.startTop;
-            }
-        // else {
-            var updatedQueues = this.printerQueues;
-            var remainingJobs;
-
-            //get jobs regular jobs
-            this.printerQueues.forEach((queue) => {
-                if (queue.device == lastDevice) {
-                    remainingJobs = queue.jobs.filter((job) => {
-                        return (job.priority != "New");
-                    });
-                }
-            });
-
-            console.log("remaining job count: " + remainingJobs.length);
-
-            //update previous and new queue
-            updatedQueues.forEach((queue) => {
-                if (queue.device == lastDevice) {
-                    queue.jobs = remainingJobs;
-                }
-                if (queue.device == newDevice) {
-                    queue.jobs.push(this.newJob);
-                }
-            });
-            // this.printerQueues = updatedQueues;
-            this.$store.dispatch("updateQueues", updatedQueues);
-        // }
-
-
-            this.$store.dispatch("updateNewJob", updatedJob.date);
-
-            //RESET Z-Index after animation
-            const jobStyle = el.style;
-            jobStyle.zIndex = "0";
-            
-            this.startTimeHumanReadable = '';
-            
-            return;
-        }
-        // el.style.left = l + deltaX + 'px';
-        //Update date
-        
-        el.style.left = l + deltaX + 'px';
-        el.style.top = t + deltaY + 'px';
-        this.humanReadableTime(parseFloat(el.style.left));
+            el.style.left = l + deltaX + 'px';
+            el.style.top = t + deltaY + 'px';
+            this.humanReadableTime(parseFloat(el.style.left));
         }
     },
     computed: {
         ...mapState(['sliderValue', 'printerQueues', 'newJob', 'modalVisibility']),
         ...mapGetters(['username', 'is_authenticated', 'userId']),
+        myJob : function() {
+            return  (this.selectedJob!=-1 && this.selectedJob.user==this.userId);
+        },
+        cancelable: function() {
+            return (this.selectedJob!=-1 && this.reservationInProcess==false && this.selectedJob.user==this.userId);
+        },
         computedPrinterQueues: function() {
             const computedPrinterQueue = this.printerQueues;
             computedPrinterQueue.forEach(printerQueue => {
